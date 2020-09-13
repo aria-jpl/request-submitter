a68355dd8ddc75df87022c1510d083d3ee170f9d
Mohammed Karim <widewidesea2017@gmail.com>
2020-09-13 07:02:12 -0700

    ARIA-446

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # clone spyddder-man to be moved to its final location by docker builder
-git clone -b develop https://github.com/hysds/multi_acquisition_localizer.git
+git clone -b ARIA-446 https://github.com/hysds/multi_acquisition_localizer.git
 
 # clone ariamh to be moved to its final location by docker builder
-git clone -b develop https://github.com/hysds/ariamh.git
+git clone -b ARIA-446 https://github.com/hysds/ariamh.git
 
